9c6f0b18c2ba678cf18ac401f8c1d34b158dfcbd
THUANPHAN\phang <thuanpg159@gmail.com>
2024-03-06 01:35:48 +0700

    fix logic

--- app/home/configs/constants.ts
+++ app/home/configs/constants.ts
@@ -1,6 +1,6 @@
 import { ENVIRONMENT } from "@/app/config/constants";
 
 export const ENDPOINT =
-  ENVIRONMENT === "development"
+  ENVIRONMENT === "production"
     ? "https://social-media-server-w8cl.onrender.com:8000/"
     : "http://localhost:8000";
