a3d0b33438dd0a03e79a213ad21d8ba30c63af1f
THUANPHAN\phang <thuanpg159@gmail.com>
2024-03-06 01:52:09 +0700

    remove port from url

--- app/home/configs/constants.ts
+++ app/home/configs/constants.ts
@@ -2,5 +2,5 @@ import { ENVIRONMENT } from "@/app/config/constants";
 
 export const ENDPOINT =
   ENVIRONMENT === "production"
-    ? "https://social-media-server-w8cl.onrender.com:8000/"
+    ? "https://social-media-server-w8cl.onrender.com/"
     : "http://localhost:8000";
--- app/socket.ts
+++ app/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://social-media-server-w8cl.onrender.com:8000/"
+    ? "https://social-media-server-w8cl.onrender.com/"
     : "http://localhost:8000";
 
 export const socket = io(URL);
